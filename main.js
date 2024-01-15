bbbc19dbd2f249cba7411fc5e8629a8501a602c3
sanjeevchandru <chandrusanjeev5@gmail.com>
2024-01-15 09:22:21 +0530

    15-01-p109

---
+++ main.js
@@ -0,0 +1,5 @@
+document.write("73.to reverse the elements of a given array of integers of length 3."+"<br>");
+function test73(arr){
+    return arr.reverse();
+}
+document.write("The array :[1,2,3] , Ans :"+test73([1,2,3])+"<br><br>");
